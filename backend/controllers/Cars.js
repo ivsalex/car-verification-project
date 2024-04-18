7a7f75dacb6f1f4582c934b48dfae74c75674b58
ivsalex <ivsalex@yahoo.com>
2024-04-18 12:23:37 +0300

    Added new cars routes and functions

--- backend/controllers/Cars.js
+++ backend/controllers/Cars.js
@@ -5,7 +5,7 @@ const Car = require('../models/Car');
 
 exports.carCreate = async (req, res, next) => {
     try {
-        const checkedCars = await Car.find({ vin: req.body.vin });
+        const checkedCars = await Car.find({ carVin: req.body.carVin });
         if (checkedCars.length >= 1) {
             return res.status(500).json({
                 message: 'This VIN is already in use!'
@@ -55,6 +55,54 @@ exports.getAllCars = async (req, res, next) => {
             cars: cars
         });
     } catch (err) {
+        res.status(500).json({
+            error: err
+        });
+    }
+};
+
+exports.getCarById = async (req, res, next) => {
+    try {
+        const id = req.params.carId;
+        const doc = await Car.findById(id);
+
+        if (doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({ message: 'No valid entry for that ID' });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err });
+    }
+};
+
+exports.carDelete = async (req, res, next) => {
+    try {
+        const id = req.params.carId;
+        const result = await Car.deleteOne({ _id: id });
+
+        res.status(200).json(result);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    }
+};
+
+exports.carModify = async (req, res, next) => {
+    try {
+        const id = req.params.carId;
+        const updateOps = {};
+        for (const ops of req.body) {
+            updateOps[ops.propName] = ops.value;
+        }
+        const result = await Car.updateOne({ _id: id }, { $set: updateOps });
+
+        res.status(200).json(result);
+    } catch (err) {
+        console.log(err);
         res.status(500).json({
             error: err
         });
