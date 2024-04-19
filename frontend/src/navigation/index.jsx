17b9b99d1da01bb8e5a4e538ad1af153372b1eda
ivsalex <ivsalex@yahoo.com>
2024-04-19 11:54:22 +0300

    Added Login Card, Login Page and Login Function

--- frontend/src/navigation/index.jsx
+++ frontend/src/navigation/index.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Cars from "../components/views/Cars";
 import Car from "../components/views/Car";
 import { Navbar } from "../components/elements/Navbar";
+import { Login } from "../components/views/Login";
 
 const Navigation = () => {
     return (
@@ -11,6 +12,7 @@ const Navigation = () => {
                 <Route path="*" element={<Navigate to="/" replace />} />
                 <Route path="/cars" element={<Cars />} />
                 <Route path="/cars/:carId" element={<Car />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
     );
