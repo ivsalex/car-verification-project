be658396a9a5dfb4ec9fc9e5d436c277f3592de3
ivsalex <ivsalex@yahoo.com>
2024-04-29 14:36:26 +0300

    modified cors origin

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://itp-rca.onrender.com/');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') {
