acfe9025a53433ec48bd429ce38419723d1b1011
ivsalex <ivsalex@yahoo.com>
2024-07-17 23:10:15 +0300

    error fix

--- backend/routes/Cars.js
+++ backend/routes/Cars.js
@@ -5,7 +5,7 @@ const { ClerkExpressRequireAuth } = require('@clerk/clerk-sdk-node');
 const CarsController = require('../controllers/Cars');
 
 //Get all Cars Route
-router.get('/', ClerkExpressRequireAuth, CarsController.getAllCars);
+router.get('/', ClerkExpressRequireAuth(), CarsController.getAllCars);
 
 //Get all Cars Route
 router.get('/expiring', ClerkExpressRequireAuth, CarsController.getAllExpiringCars);
