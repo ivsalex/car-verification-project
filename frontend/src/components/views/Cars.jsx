40d2fed86f14b9481dc17186a54869a213e3dd1d
ivsalex <ivsalex@yahoo.com>
2024-07-19 20:37:51 +0300

    improvements

--- frontend/src/components/views/Cars.jsx
+++ frontend/src/components/views/Cars.jsx
@@ -47,6 +47,16 @@ const Cars = () => {
         } catch (error) {
             console.error('Error fetching data:', error);
         }
+
+        if (error.response) {
+            console.error('Response data:', error.response.data);
+            console.error('Response status:', error.response.status);
+            console.error('Response headers:', error.response.headers);
+        } else if (error.request) {
+            console.error('Request data:', error.request);
+        } else {
+            console.error('Error message:', error.message);
+        }
     };
 
     const deleteCar = async (carId) => {
