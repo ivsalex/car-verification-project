d449cd4f75033d2255c3714efedfc663a334a30c
ivsalex <ivsalex@yahoo.com>
2024-07-22 19:37:01 +0300

    modified cron job

--- backend/cron.js
+++ backend/cron.js
@@ -1,21 +1,9 @@
-const { CronJob } = require('cron');
-const https = require('https');
+const cron = require('node-cron');
 
-const backendUrl = 'https://api.ivaiondan.ro';
-const job = new CronJob('*/14 * * * *', function () {
-    console.log('Restarting server!');
-
-    https.get(backendUrl, (res) => {
-        if (res.statusCode === 200) {
-            console.log('Server restarted!');
-        } else {
-            console.error(
-                `Failed to restart server with status code ${res.statusCode}`
-            );
-        }
-    }).on('error', (err) => {
-        console.error('Error during Restart:', err.message)
-    });
+cron.schedule('*/14 * * * *', function () {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    console.log(`Server restarted at: ${hours}:${minutes}:${seconds}`);
 });
-
-module.exports = job
