ab311b55e33fbb077acdfb00235fcf062d579cb3
ivsalex <ivsalex@yahoo.com>
2024-07-22 12:13:07 +0300

    modified cars file

--- frontend/src/components/views/Cars.jsx
+++ frontend/src/components/views/Cars.jsx
@@ -14,9 +14,8 @@ const Cars = () => {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + { token }
+                    'Authorization': `Bearer ${await getToken()}`
                 },
-                credentials: 'include',
                 mode: 'cors'
             });
 
@@ -33,25 +32,6 @@ const Cars = () => {
             console.error('Error fetching data:', error);
         }
     }
-
-    // const fetchCarsData = async () => {
-    //     try {
-    //         const token = await getToken();
-    //         const config = {
-    //             headers: { Authorization: `Bearer ${token}` }
-    //         };
-    //         const response = await axios.get('https://api.ivaiondan.ro/cars', config, {
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 'authorization': `Bearer ${token}`
-    //             },
-    //         });
-
-    //         setCars(response.data?.cars);
-    //     } catch (error) {
-    //         console.error('Error fetching data:', error);
-    //     }
-    // };
 
     const deleteCar = async (carId) => {
         try {
