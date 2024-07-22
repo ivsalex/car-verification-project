15595489e9973f5947c25ec4ce980f3c5c834485
ivsalex <ivsalex@yahoo.com>
2024-07-22 14:48:02 +0300

    build

--- frontend/src/components/views/Login.jsx
+++ frontend/src/components/views/Login.jsx
@@ -4,7 +4,7 @@ import { SignIn } from '@clerk/clerk-react';
 function LoginPage() {
     return (
         <div>
-            <SignIn path='/login' />
+            <SignIn />
         </div >
     );
 }
