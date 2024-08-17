f27bd3245ad4328fbcaba12a5ea6b12d05e0b936
ivsalex <ivsalex@yahoo.com>
2024-08-17 21:37:42 +0300

    Modified Search

--- frontend/src/components/elements/Search.jsx
+++ frontend/src/components/elements/Search.jsx
@@ -25,18 +25,19 @@ function SearchInput({ onSearch }) {
 
     return (
         <div className="flex justify-center items-center">
-            <form onSubmit={handleSubmit} className="flex items-center">
+            <form onSubmit={handleSubmit} className="flex items-center border border-gray-300 rounded-lg overflow-hidden">
                 <input
                     type="text"
                     placeholder="Exemplu: BV01TST"
                     value={searchTerm}
                     onChange={handleChange}
-                    className='border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500'
+                    className='px-3 py-2 focus:outline-none focus:border-blue-500 flex-grow'
+                    style={{ border: 'none' }}
                 />
                 {!isSearchDone && (
                     <button
                         type="submit"
-                        className="ml-2 px-2 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                        className="mx-2 px-2 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
                     >
                         <SearchIcon className="h-4 w-4" />
                     </button>
@@ -45,7 +46,7 @@ function SearchInput({ onSearch }) {
                     <button
                         type="button"
                         onClick={handleClear}
-                        className="ml-2 px-2 py-2 bg-gray-200 text-gray-600 rounded-full hover:bg-gray-300 focus:outline-none"
+                        className="mx-2 px-2 py-2 bg-gray-200 text-gray-600 rounded-full hover:bg-gray-300 focus:outline-none"
                     >
                         <XIcon className="h-4 w-4" />
                     </button>
