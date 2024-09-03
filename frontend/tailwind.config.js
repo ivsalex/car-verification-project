92550d4e68c46df11ff95e1e288594949e285926
ivsalex <ivsalex@yahoo.com>
2024-09-03 21:08:15 +0300

    Added Popup Message for sms notifications

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,11 +13,22 @@ module.exports = {
                 '86': '390px',
                 '98': '32em',
                 '100': '38rem',
-                '120': '50rem',
+                '120': '50rem'
             },
-            width: {
-                'table': ''
-            }
+            keyframes: {
+                slideInRight: {
+                    '0%': { transform: 'translateX(100%)', opacity: '0' },
+                    '100%': { transform: 'translateX(0)', opacity: '1' },
+                },
+                slideOutRight: {
+                    '0%': { transform: 'translateX(0)', opacity: '1' },
+                    '100%': { transform: 'translateX(100%)', opacity: '0' },
+                },
+            },
+            animation: {
+                slideInRight: 'slideInRight 0.5s ease-out forwards',
+                slideOutRight: 'slideOutRight 0.5s ease-in forwards',
+            },
         },
     },
     plugins: [],
